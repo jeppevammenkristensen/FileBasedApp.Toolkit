90349b71f4d56d14034eac94c4806fbd89c33add
Jeppe Roi Kristensen <jeppe@jeppek.dk>
2026-03-05 01:03:44 +0100

    Work on filebased app template

---
+++ Templates/FileBasedAppTemplate/FileBasedToolkitApp.cs
@@ -0,0 +1,50 @@
+#:package FilebasedApp.Toolkit@*
+#:property PublishAot=false 
+
+using Spectre.Console.Cli;
+using TruePath;
+using Spectre.Console;
+using FileBasedApp.Toolkit;
+using System.IO.Abstractions;
+
+var commandApp = new CommandApp<RunCommand>();
+await commandApp.RunAsync(args);
+
+
+public class RunCommand : AsyncCommand<RunCommand.Settings> // For sync only you can use Command (and have Execute instead of ExecuteAsync
+{
+	public override async Task<int> ExecuteAsync(CommandContext context, Settings settings, CancellationToken cancellationToken)
+	{
+		AnsiConsole.MarkupLineInterpolated($"[green]SomePath is {settings.SomePathAbsolute.Value}[/]");
+		
+		var parentDirectory = settings.SomePathAbsolute / "..";
+		
+		// Uses the extensions method to GetFiles
+		foreach(var csfile in parentDirectory.EnumerateFiles("*.cs", SearchOption.AllDirectories).Take(50))
+		{
+			AnsiConsole.MarkupLineInterpolated($"[dim]Relative path {csfile.RelativeTo(settings.SomePathAbsolute)}[/]");
+		}
+		
+		await SimpleExec.Command.RunAsync("dotnet", ["--version"], parentDirectory.Value);
+		return 0; // 0 for success
+	}
+
+	public class Settings : ExtendedCommandSettings
+	{
+		[CommandArgument(0, "[SomePath]")]
+		public string? SomePath { get; set; }
+
+		/// <summary></summary>
+		public AbsolutePath SomePathAbsolute { get; private set; }
+		protected override ValidationResult DoValidate()
+		{
+			// Exceptions here will bubble up and outputted as validation		
+
+			// This will evaluate the path. If the path is relative, it will relative (in this case) against the execution folder. That would be the
+			// directory that this .cs lives in
+			SomePathAbsolute = this.TryGetDirectory(SomePath, PredefinedRootPath.ExecutionFolder, allowEmpty: true, shouldExist: false);
+
+			return base.DoValidate();
+		}		
+	}
+}

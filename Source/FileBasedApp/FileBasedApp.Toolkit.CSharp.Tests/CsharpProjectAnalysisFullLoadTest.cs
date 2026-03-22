3550af3292f7c68039d7b8b73b2b494ba146fab5
Jeppe Roi Kristensen <jeppe@jeppek.dk>
2026-03-23 00:10:30 +0100

    Add FindImplementationOfInterface methods to CompilationExtensions and CsharpProjectAnalysis
    Introduce methods to find all named type symbols implementing a specified interface by fully qualified name or ITypeSymbol. Add GetNamedTypeSymbolsIncludingReferences/GetNamedTypeSymbolForCurrentAssembly methods to enumerate named types from compilation and current assembly. Extract GetNamedTypeSymbolsVisitor into separate file. Add FindRequiredTypeByMetadataName helper with optional multiple-match validation. Include comprehensive tests with test project modifications (ISomeInterface/Someclass) and new partial test class for interface finding scenarios.

--- Source/FileBasedApp/FileBasedApp.Toolkit.CSharp.Tests/CsharpProjectAnalysisFullLoadTest.cs
+++ Source/FileBasedApp/FileBasedApp.Toolkit.CSharp.Tests/CsharpProjectAnalysisFullLoadTest.cs
@@ -1,8 +1,13 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.IO.Abstractions;
+using System.Linq;
 using System.Threading.Tasks;
+using FileBasedApp.Toolkit.CSharp.Extensions;
 using FluentAssertions;
 using JetBrains.Annotations;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.FindSymbols;
 using TruePath;
 using Xunit;
 
@@ -10,7 +15,7 @@ namespace FileBasedApp.Toolkit.CSharp.Tests;
 
 [TestSubject(typeof(CsharpProjectAnalysis))]
 [Trait("Category", "Slow")]
-public class CsharpProjectAnalysisFullLoadTest : IAsyncLifetime
+public partial class CsharpProjectAnalysisFullLoadTest : IAsyncLifetime
 {
     private CsharpProjectAnalysis _analysis;
 
@@ -21,6 +26,45 @@ public class CsharpProjectAnalysisFullLoadTest : IAsyncLifetime
         _analysis.Project.Should().NotBeNull();
         _analysis.MsBuildWorkspace.Should().NotBeNull();
     }
+
+    public const int UniqueNamedTypesInTestProject = 2;
+    
+    
+    [Fact]
+    public void GetNamedTypeSymbols_ReturnsExpected()
+    {
+        var matches = _analysis.Compilation.GetNamedTypeSymbolsIncludingReferences()
+            .ToList();
+        
+        matches.Should().HaveCountGreaterThan(UniqueNamedTypesInTestProject);
+        matches.Where(x => SymbolEqualityComparer.Default.Equals(x.ContainingAssembly, _analysis.Compilation.Assembly))
+            .Should().HaveCount(UniqueNamedTypesInTestProject, $"There are {UniqueNamedTypesInTestProject} unique named types in the test project. If this has changed update the {nameof(UniqueNamedTypesInTestProject)} constant");
+    }
+    
+    [Fact]
+    public void GetNamedTypeSymbolsForCurrentAssembly_ReturnsExpected()
+    {
+        var matches = _analysis.Compilation.GetNamedTypeSymbolForCurrentAssembly()
+            .ToList();
+        
+        matches.Should().HaveCount(UniqueNamedTypesInTestProject, $"There are {UniqueNamedTypesInTestProject} unique named types in the test project. If this has changed update the {nameof(UniqueNamedTypesInTestProject)} constant");
+    }
+    
+    [Fact]
+    public void FindImplementationOfInterface_ReturnsExpected()
+    {
+        var matches = _analysis.Compilation.GetNamedTypeSymbolForCurrentAssembly()
+            .ToList();
+
+        var @if = matches.SingleOrDefault(x => x.Name == "ISomeInterface");
+
+        matches
+            .Where(x => !x.Equals(@if, SymbolEqualityComparer.Default))
+            .Where(x => x.AllInterfaces.Contains(@if, SymbolEqualityComparer.Default))
+            .Should().HaveCount(1).And.Subject.First().Name.Should().Be("Someclass");
+        
+    }
+    
     
     
     /// <summary>
---
+++ Source/FileBasedApp/FileBasedApp.Toolkit.CSharp/Extensions/GetNamedTypeSymbolsVisitor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace FileBasedApp.Toolkit.CSharp.Extensions;
+
+internal class GetNamedTypeSymbolsVisitor : BaseSymbolVisitor<INamedTypeSymbol>
+{
+    private readonly Action<INamedTypeSymbol> _action;
+
+    
+    protected override void HandleNamedTypeSymbol(INamedTypeSymbol symbol)
+    {
+        ExecuteMatched(symbol);
+    }
+}
